937adb52d178a4cea3f426360b427525ba35cae0
adedotun <erik.edwards.jon@gmail.com>
2025-09-08 18:43:32 -0700

    update

---
+++ Acceleration.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	public class Acceleration{
+		public static void main(String[] args){
+			Scanner input = new Scanner(System.in);
+			System.out.print("enter the value for V0:");
+			double v0 = input.nextDouble();
+			System.out.print("enter the value for v1:");
+			double v1 = input.nextDouble();
+			System.out.print("enter the value for t:");
+			double t = input.nextDouble();
+			double a = (v1 - v0) / t;
+			System.out.println("The average acceleration is" + a);
+
+}
+}
