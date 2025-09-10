21d7dd11b5dc4ba1a301e122f9350d92443aa0a5
adedotun <erik.edwards.jon@gmail.com>
2025-09-09 18:23:07 -0700

    update

--- TASK/Username.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Username{
-		public static void main(String[] args){
-			Scanner scanner = new Scanner(System.in);
-			System.out.print("enter username:");
-			String name = scanner.nextLine();
-			System.out.printf("my name is %s%n",name);
-}
-}
