93511530d10f34cb625f49e0334fa869530958d5
adedotun <erik.edwards.jon@gmail.com>
2025-09-12 13:43:58 -0700

    push

--- java/assignment/chapter 2 ditel/CharacterSet.java
+++
@@ -1,8 +0,0 @@
-public class CharacterSet{
-	public static void main(String[] args){
-char  alpha = 'A'  ;
-
-//System.out.printf("The character %c%c%c%c%c%c%c%c%c%c%c%c%c%c has the value %d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n%d%n", alpha, 'B', 'C', 'a', 'b', 'c', '0', '1' ,'2' ,'$' ,'*' ,'+', '/' ,' '
-}
-}
-
