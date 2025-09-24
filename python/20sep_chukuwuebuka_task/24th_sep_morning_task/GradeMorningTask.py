52145bee4f751d84ad1f189d32781ddea0f9eda2
adedotun <erik.edwards.jon@gmail.com>
2025-09-24 11:22:33 -0700

    update

--- python/20sep_chukuwuebuka_task/24th_sep_morning_task/GradeMorningTask.py
+++ python/20sep_chukuwuebuka_task/24th_sep_morning_task/GradeMorningTask.py
@@ -1,41 +1,39 @@
 
-#declare  variable (count) and initialize a value of 3 to it
-#declare  variable(sum) and initialize a value of 0 to it
-#use iteration condition that runs 3 times to ask user for score, save the score in a variable  #and check if the score is;
-#1. greater than or equals to 0 and lesser than equals to 100, if not
-#enter a valid input, but if it is right , the addition of sum and count should be saved in #variable total, then;
-#check if the score is greater than or equals to 90 and  if the score is les than or equals to #100, and display "the score  and the grade is A" if not
-#check if the score is greater than or equals to 80 and  if the score is less than  90, and #display "the score  and the grade is B" if not,
-#check if the score is greater than or equals to 70 and  if the score is less than  80, and #display "the score  and the grade is C", if not,
-#check if the score is greater than or equals to 60 and  if the score is less than  70, and #display "the score  and the grade is D"if not,
-#display "the score and the grade is F"
-#when the condition exceeds 3 iteration, the cycle ends
-#reinitialize count to be 3 then display the result of sum divided by count.
+"""
+declare a variable (count) and initialize a value of 0 to it
+declare  variable(score) and initialize a value of 0 to it
+use iteration condition that runs 3 times to ask user for score, save the score in a variable  and check if the score is;
+ check if the score is more than or same as 90 and  if the score is smaller than or same to 100, and display grade of average is A and the average score  if not
+check if the score is more than or same as 80 and  if the score is samller than  90, and display grade of average is B and the average score if not,
+check if the score is Bigger than or same  to 70 and  if the score is smaller 80, and display grade of average is C and the average score  if not,
+check if the score is bigger than  or same to 60 and  if the score is smaller than  70, and display grade of average is D and the average score if not,
+display grade of average is F and the average score 
+"""
 
-count = 3
+ 
+
+count = 0
 total = 0
-while count > 0:
+while count < 3:
 	score = int(input("enter score: "))
-	if score >= 0 and score <= 100:
-		total += score
-		if score >= 90 and score <= 100:
-			print("The score", score ,"and the grade is A")
-		elif score >= 80 and score <= 90:
-			print("The score", score ,"and the grade is B")
-		elif score >= 70 and score <= 80:
-			print("The score", score ,"and the grade is C")
-		elif score >= 60 and score <= 70:
-			print("The score", score ,"and the grade is B")
-		else:
-			print("The score", score ,"and the grade is F")
+	total += score
+	count+=1
+
+	
+
+total/=3
+
+if total >= 90 and total <= 100:
+	print("The average", total ,"and the grade is A")
+elif total >= 80 and total <= 90:
+	print("The average", total ,"and the grade is B")
+elif total >= 70 and total <= 80:
+	print("The average", total ,"and the grade is C")
+elif total >= 60 and total <= 70:
+	print("The average", total ,"and the grade is D")
+else:
+	print("The average", total ,"and the grade is F")
 
 
 
-
-
-	else:
-		print("invalid input")
-
-	count-=1
-count = 3
-print(total / count)
+print(total)
