2bb31e7f32ade0a0d45a710e5bc4c02db036c77f
adedotun <erik.edwards.jon@gmail.com>
2025-09-26 03:57:03 -0700

    chibuzor task

---
+++ java/Gate1/TaskFour.java
@@ -0,0 +1,26 @@
+public class TaskFour{
+		public static void main(String [] args){
+
+			int count = 1;
+			while(count <= 10){
+				if(count % 4 == 0)
+					System.out.println("even number " + count);
+
+			count++;
+
+
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
