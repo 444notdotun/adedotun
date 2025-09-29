1359905466d692d497f30b399fa7d61d4666b2e6
adedotun <erik.edwards.jon@gmail.com>
2025-09-29 01:26:36 -0700

    push

---
+++ python/chapter3/GasMileage.py
@@ -0,0 +1,24 @@
+"""
+prompt user for gallon used in each trip and 0 to end the program, save in the input as gallon
+prompt user for miles used in each trip and save it in the input as miles.
+ save variable count to 0
+while usuing iteration the condition should be gallon greater than 0 in the body of the iteration,
+"""
+
+
+
+gallon = 0
+miles = 0
+count = 0
+trips = 0
+choice = 1
+while(choice != 0):
+	gallon = int(input("enter gallon used in a trip: "))
+	miles = int(input("enter miles: "))			
+	gasmileage = gallon / miles
+	trips += gasmileage
+	count+=1
+
+	choice =int(input("do you want continue?, 0 to exit & any number to continue: "))
+		
+	print ("for", count ,"trips this is the total gas mileage",trips)
---
+++ python/chapter3/cubic.py
@@ -0,0 +1,18 @@
+num1 = 1
+count = 1
+counter = 1
+net = 1
+print("N \t N^2 \t N^3 \t N^4 ")		
+while(count <= 5):
+	while(net <= 4):
+		num1*=counter
+		print(num1,end="\t")
+		net+=1
+							
+	print()
+	num1 =1
+	net = 1
+	counter+=1
+	count+=1
+						
+	
