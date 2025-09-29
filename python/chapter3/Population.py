8afc1a6ee2a3fc2f4d9715f26f6cfd984540a32e
adewole adedotun <erik.edwards.jon@gmail.com>
2025-09-29 04:20:43 -0700

    Update Population.py

--- python/chapter3/Population.py
+++ python/chapter3/Population.py
@@ -2,8 +2,8 @@ wp = 8142000000
 dgr = 1.0
 count = 1
 while count <= 100:
-	import math
-	population = wp * math.pow(dgr,count)
+	
+	population = wp * (dgr**count)
 	
 	print(" estimated population for year" ,count ,"is", population)
 	count +=1	
