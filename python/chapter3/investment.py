540a3e90b3c454f026d24258583d72b5632a5d57
adewole adedotun <erik.edwards.jon@gmail.com>
2025-09-29 03:53:51 -0700

    Update investment.py

--- python/chapter3/investment.py
+++ python/chapter3/investment.py
@@ -5,6 +5,6 @@ annual_rate = principal/7
 count = 1
 while count <= 30:
 
-	amount_accrued10 = principal+(1 + annual_rate)*count
+	amount_accrued10 = principal+(1 + annual_rate)**count
 	print(amount_accrued10 , "is the amount after ",count," years ")
 	count+=1
