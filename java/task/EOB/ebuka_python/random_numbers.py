e7d33618cbf18a6541e12ba43c7ac3f3aff5076d
adewole adedotun <erik.edwards.jon@gmail.com>
2025-10-10 01:45:02 -0700

    Update random_numbers.py
    Signed-off-by: adewole adedotun <erik.edwards.jon@gmail.com>

--- java/task/EOB/ebuka_python/random_numbers.py
+++ java/task/EOB/ebuka_python/random_numbers.py
@@ -94,3 +94,20 @@ for numbers in random_numbers_list:
 print(smallest)
 print(random_numbers_list)
 
+
+
+count = 0
+string = [ 'oyinade', 'emma', 'adaa' ,'nife' ,'kunle']
+for number_of_strings in  string:
+	
+	if number_of_strings == string[count]:
+		new_element = str(string[count])
+
+		if len(new_element) >= 2:
+			if new_element[0] == new_element[len(new_element)-1]:
+				print(number_of_strings)
+			else:
+				print(f' first and the last character are different')
+	
+
+	count+=1
